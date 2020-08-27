c711b45b998537eb5e35f857edfab95a5e011cff
Zvmox <62778188+Zvmox@users.noreply.github.com>
2020-08-27 05:00:52 +0300

    commit

---
+++ js/style.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        items:4,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:3
+            },
+            768:{
+                items:3
+            },
+            938:{
+                items:4
+            }
+        }
+    });
+
+    $('.chart1').easyPieChart({
+        easing: 'easInOut',
+        barColor: '#F7444E',
+        trackColor:false,
+        scaleColor:false,
+        lineWidht:4,
+        size:200,
+    });
+
+
+
+    var skillsTopOffset = $(".SkillsSection").offset().top;
+    
+    $(window).scroll(function(){
+
+        if(window.pageYOffset > skillsTopOffset - $(window).height() + 300){
+
+            $('.chart').easyPieChart({
+                easing: 'easInOut',
+                barColor: '#F7444E',
+                trackColor:false,
+                scaleColor:false,
+                lineWidht:4,
+                size:152,
+                onStep: function(from, to, percent) {
+                    $(this.el).find('.percent').text(Math.round(percent)); 
+                }
+            });
+
+        }
+
+    });
+
+
+
+});
